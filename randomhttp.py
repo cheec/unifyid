374b55a8f621e95a7e88419535af7debfe03ebe2
clarencechee <clarencechee94@gmail.com>
2017-05-20 11:55:37 +0800

    wrap parsed request in list

--- randomhttp.py
+++ randomhttp.py
@@ -13,22 +13,20 @@ def request_random(num, min_val, max_val):
         'format': 'plain',
         'rnd': 'new'
     }
-    
     req = requests.get('https://www.random.org/integers/', params=payload)
-    if req.status_code != 200: # TODO: quota checking before polling
+    if req.status_code != 200:  # TODO: quota checking before polling
         print('Error requesting bits. (' + str(req.status_code) + ')')
         exit(1)
-        
-    return np.asarray(int(x) for x in req.text.split())
+
+    return np.asarray([int(x) for x in req.text.split()])
 
 nx = ny = 128
 min_rgb = 0
-max_rgb = 2 ** 24 - 1 # rgb hex (6 bytes)
+max_rgb = 2 ** 24 - 1  # rgb hex (6 bytes)
 
 if __name__ == "__main__":
     random_rgb = request_random(nx * ny / 2, min_rgb, max_rgb)
     random_rgb = np.append(random_rgb, request_random(nx * ny / 2, min_rgb, max_rgb))
-
     random_rgb = random_rgb.reshape(128, 128)
 
     fig = plt.imshow(random_rgb)
