039519fd30c0d1d0402361c582384f18f4711bc1
clarencechee <clarencechee94@gmail.com>
2017-05-20 10:09:18 +0800

    generate 128x128 random numbers (0-255)

--- randomhttp.py
+++ randomhttp.py
@@ -1,7 +1,28 @@
 import requests
 
-payload = {'num': '10', 'min': '1', 'max': '6', 'col': '1', 'base': 10, 'format': 'plain', 'rnd': 'new'}
-req = requests.get('http://random.org./integers/', params=payload)
+import matplotlib.pyplot as plt
+import numpy as np
 
-random_integers = [int(x) for x in req.text.split()]
+def request_random(num):
+    payload = {
+    'num': num,
+    'min': '0',
+    'max': '255',
+    'col': '1',
+    'base': 10,
+    'format': 'plain',
+    'rnd': 'new'
+    }
 
+    req = requests.get('http://random.org./integers/', params=payload)
+    return np.asarray([int(x) for x in req.text.split()])
+
+if __name__ == "__main__":
+    nx = ny = 128
+
+    random_integers = request_random(str(nx * ny / 2))
+    random_integers = np.append(random_integers, request_random(str(nx * ny / 2)))
+    
+    plt.plot(random_integers)
+    plt.axis
+    plt.show()
