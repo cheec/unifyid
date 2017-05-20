68febe7ed675098596cf6ead2aee9ba02502e3b8
clarencechee <clarencechee94@gmail.com>
2017-05-20 11:33:04 +0800

    generate 128x128 bitmap

--- randomhttp.py
+++ randomhttp.py
@@ -3,26 +3,32 @@ import requests
 import matplotlib.pyplot as plt
 import numpy as np
 
-def request_random(num):
+def request_random(num, min_val, max_val):
     payload = {
-    'num': num,
-    'min': '0',
-    'max': '255',
-    'col': '1',
-    'base': 10,
-    'format': 'plain',
-    'rnd': 'new'
+        'num': str(num),
+        'min': str(min_val),
+        'max': str(max_val),
+        'col': '1',
+        'base': 10,
+        'format': 'plain',
+        'rnd': 'new'
     }
-
     req = requests.get('http://random.org./integers/', params=payload)
-    return np.asarray([int(x) for x in req.text.split()])
+    return np.asarray(req.text.split())
+
+nx = ny = 128
+min_rgb = 0
+max_rgb = 2 ** 24 - 1 # rgb hex (6 bytes)
 
 if __name__ == "__main__":
-    nx = ny = 128
+    random_rgb = request_random(nx * ny / 2, min_rgb, max_rgb)
+    random_rgb = np.append(random_rgb, request_random(nx * ny / 2, min_rgb, max_rgb))
 
-    random_integers = request_random(str(nx * ny / 2))
-    random_integers = np.append(random_integers, request_random(str(nx * ny / 2)))
-    
-    plt.plot(random_integers)
-    plt.axis
+    random_rgb = random_rgb.reshape(128, 128)
+
+    fig = plt.imshow(random_rgb)
+    fig.set_cmap('jet')
+    fig.axes.get_xaxis().set_visible(False)
+    fig.axes.get_yaxis().set_visible(False)
     plt.show()
+
